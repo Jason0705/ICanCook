f82c7b3c7edc94bb23239d2b9fef07c7af4cc4fd
chanroyc <chanroyc@sfu.ca>
2016-11-18 21:33:02 -0800

    Fixed Recipe Query sets in Models.py; added auto-increment recipe IDs

--- mysite/recipes/models.py
+++ mysite/recipes/models.py
@@ -5,14 +5,14 @@ from django.db import models
 # Create your models here.
 
 class Recipe(models.Model):
-	rid = models.IntegerField(primary_key=True)
+	rid = models.AutoField(primary_key=True)
 	title = models.CharField(max_length=100)
 	description = models.CharField(max_length=500)
 	userid = models.IntegerField()
 	prep_time = models.IntegerField()
 
 	def __str__(self):
-		return ("%i, %s, %s, %i, %i" % (self.rid, self.title, self.description, self.userid, self.prep_time))
+		return ("RID: %i, TITLE: %s" % (self.rid, self.title))
 
 class Step(models.Model):
 	rid = models.ForeignKey(Recipe, on_delete=models.CASCADE)
@@ -20,14 +20,14 @@ class Step(models.Model):
 	order = models.CharField(max_length=25)
 
 	def __str__(self):
-		return self.rid
+		return ("DESCRIPTION: %s, ORDER: %s" %(self.description, self.order))
 
 class QuantityType(models.Model):
 	qid = models.IntegerField(primary_key=True)
 	name = models.CharField(max_length=25)
-
+	
 	def __str__(self):
-		return self.name
+		return ("QID: %i, NAME: %s" % (self.qid, self.name))
 
 class Ingredient(models.Model):
 	rid = models.ForeignKey(Recipe, on_delete=models.CASCADE)
@@ -36,4 +36,4 @@ class Ingredient(models.Model):
 	quantity_type = models.ForeignKey(QuantityType)
 
 	def __str__(self):
-		return self.name
+		return ("NAME: %s, QUANTITY: %s" % (self.name, self.quantity))
