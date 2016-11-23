81f118ecdecb9fc3962e5f8617c2d8ad98ed6cb2
Jitin Dhillon <jitind@sfu.ca>
2016-11-22 18:54:32 -0800

    auto format

--- mysite/recipes/urls.py
+++ mysite/recipes/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^add/$', views.add_recipe, name='add'),
-	url(r'(?P<rid>[0-9]+)/edit/$', views.edit, name='edit'),
-	url(r'(?P<rid>[0-9]+)/delete/$', views.delete, name='delete'),
-	url(r'(?P<rid>[0-9]+)/$', views.details, name='details'),
+    url(r'^$', views.index, name='index'),
+    url(r'^add/$', views.add_recipe, name='add'),
+    url(r'(?P<rid>[0-9]+)/edit/$', views.edit, name='edit'),
+    url(r'(?P<rid>[0-9]+)/delete/$', views.delete, name='delete'),
+    url(r'(?P<rid>[0-9]+)/$', views.details, name='details'),
 ]
