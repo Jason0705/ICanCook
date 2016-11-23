bf3e1f8b7ea8a4213770d8f510cd182fc6d58580
Jitin Dhillon <jitind@sfu.ca>
2016-11-22 18:32:50 -0800

    Added ability to change password.

--- mysite/user/forms.py
+++ mysite/user/forms.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 from django.forms import ModelForm, CharField, PasswordInput, HiddenInput, Form, TextInput, EmailField
 
@@ -24,3 +26,40 @@ class UpdateUserForm(Form):
     first_name = CharField(widget=BOOTSTRAP_TEXT_INPUT, label='First Name', max_length=32, required=True)
     last_name = CharField(widget=BOOTSTRAP_TEXT_INPUT, label='Last Name', max_length=32, required=True)
     email = EmailField(widget=BOOTSTRAP_TEXT_INPUT, label='Email', max_length=64, required=True)
+
+
+class UpdatePasswordForm(Form):
+    current_password = CharField(widget=BOOTSTRAP_PASS_INPUT, label='Current Password', max_length=32, required=True)
+    new_password = CharField(widget=BOOTSTRAP_PASS_INPUT, label='New Password', max_length=32, required=True)
+    new_password_confirm = CharField(widget=BOOTSTRAP_PASS_INPUT, label='Confirm New Password', max_length=32, required=True)
+    username = ''
+
+    def __init__(self, *args, **kwargs):
+        self.username = kwargs.pop('username')
+        super(UpdatePasswordForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        form_data = self.cleaned_data
+
+        current_password = form_data['current_password']
+        user = authenticate(username=self.username, password=current_password)
+
+        if user is None:
+            del form_data["current_password"]
+            del form_data["new_password"]
+            del form_data["new_password_confirm"]
+
+            self.add_error('current_password', "Current password is incorrect")
+            return form_data
+
+        new_password = form_data['new_password']
+        new_password_confirm = form_data['new_password_confirm']
+
+        if new_password != new_password_confirm:
+            del form_data["current_password"]
+            del form_data["new_password"]
+            del form_data["new_password_confirm"]
+
+            self.add_error("new_password_confirm", "Passwords do not match")
+
+        return form_data
