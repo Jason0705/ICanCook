0a8807a469d9117e8a26318e6a8bbba8e9e2f4a6
pslam <pslam@sfu.ca>
2016-11-26 19:39:05 -0800

    Added search functionality

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.db.models import Q
+
+from recipes.models import Recipe, Ingredient
 
 def index(request):
+    query = request.GET.get("q")
+    queryset_list  = Recipe.objects.all()
+    if query and request.GET:
+        queryset_list = queryset_list.filter(
+		    Q(title__icontains=query) | 
+		    Q(description__icontains=query))
+			# Still need to add ingredients searching once it can link to recipes through RID
+        context = {'queryset_list': queryset_list}
+        return render(request, 'home/search.html', context)
     return render(request, 'home/index.html')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     url(r'^home/', include('home.urls', namespace="home")),
     url(r'^user/', include('user.urls', namespace="user")),
     url(r'^login/', include('user.urls', namespace="user")),
-    url(r'^recipes/', include('recipes.urls', namespace="user")),
+    url(r'^recipes/', include('recipes.urls', namespace="recipes")),
 ]
